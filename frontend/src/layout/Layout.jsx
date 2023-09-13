d1927635ebd843468de3450a5d77942293b4da4c
Natan Andrade <natan.andrade@madeiramadeira.com.br>
2023-09-13 15:36:13 -0300

    update: removendo o footer da page Home

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,15 +1,20 @@
+import { useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Routers from "../routes/Routers";
 import Footer from "../components/Footer/Footer";
 
 const Layout = () => {
+  const location = useLocation();
+  const isHomeScreen =
+    location.pathname === "/" || location.pathname === "/home";
+
   return (
     <>
       <Header />
       <main>
         <Routers />
       </main>
-      <Footer/>
+      {!isHomeScreen && <Footer />}
     </>
   );
 };
