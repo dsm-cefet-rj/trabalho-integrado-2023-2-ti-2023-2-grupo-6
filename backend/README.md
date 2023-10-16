82b592b87f86d1f3a3692321eb09e938a4b7c94b
gabrielrodrig0 <gabrielrodrigonaga@gmail.com>
2023-10-15 22:31:14 -0300

    Criando conexão com o banco de dados

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+### Para fazer o uso do backend é preciso que o usuário crie um arquivo .env na diretório global da pasta backend com os seguintes campos: 
+
+DB_USER = "COLOQUE O USUÁRIO DO BANCO AQUI"
+DB_PW = "COLOQUE A SENHA DO BANCO AQUI"
+DB_NAME = "COLOQUE NO NOME DO BANCO AQUI"
+PORT = "INSIRA UM INTEIRO INDICANDO A PORTA"
+SECRET = "COLOQUE QUALQUER STRING AQUI PARA FAZER FUNCIONAR O JWT"
---
+++ backend/database/config.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require("dotenv").config
+
+//Conexão com o atlas (PODE TROCAR PARA OUTRO OU USAR O LOCALHOST)
+const atlasConnection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.w0bynd1.mongodb.net/e-stud?retryWrites=true&w=majority`;
+
+const connection = mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('Conexão ao banco realizada com sucesso!');
+    })
+    .catch((err) => {
+      console.error('Erro ao conectar ao MongoDB: ' + err);
+    });
+
+module.exports = connection;
