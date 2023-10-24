27197d4b8662a3ec3195115cf7f8b43f154005ca
Natan Andrade <balthazarnatan@gmail.com>
2023-10-23 22:53:05 -0300

    update: database name

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
-require("dotenv").config
+const mongoose = require("mongoose");
+require("dotenv").config;
 
 //Conexão com o atlas (PODE TROCAR PARA OUTRO OU USAR O LOCALHOST)
 const atlasConnection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.w0bynd1.mongodb.net/e-stud?retryWrites=true&w=majority`;
 
-const connection = mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`, {
+const connection = mongoose
+  .connect(`mongodb://127.0.0.1:27017/estud`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-    .then(() => {
-      console.log('Conexão ao banco realizada com sucesso!');
-    })
-    .catch((err) => {
-      console.error('Erro ao conectar ao MongoDB: ' + err);
-    });
+  .then(() => {
+    console.log("Conexão ao banco realizada com sucesso!");
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao MongoDB: " + err);
+  });
 
 module.exports = connection;
